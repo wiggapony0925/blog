0be72ec859833672391aae251697355e494601c5
wiggapony0925 <ninjeff06@gmail.com>
2023-03-27 21:17:43 -0400

    footer,header,masthead

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,5 @@
 export const SITE_TITLE = 'What I use - Takuya Matsuyama'
 export const SITE_DESCRIPTION = 'A curated list of the tech I use'
 export const HOMEPAGE_URL = 'https://www.craftz.dog/'
+
+//changed to my name did it cause error?
