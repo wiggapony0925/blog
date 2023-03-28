aa565d8c537d3c45a6390e4c158bf6ac447b5b59
wiggapony0925 <ninjeff06@gmail.com>
2023-03-27 22:42:54 -0400

    main frame

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'What I use - Takuya Matsuyama'
-export const SITE_DESCRIPTION = 'A curated list of the tech I use'
-export const HOMEPAGE_URL = 'https://www.craftz.dog/'
+export const SITE_TITLE = 'Blog % Updates - Jeffrey Fernandez'
+export const SITE_DESCRIPTION = 'Jeffreys Blog & projects'
+export const HOMEPAGE_URL = 'https://jeffreyfernandezportfolio.vercel.app'
 
 //changed to my name did it cause error?
